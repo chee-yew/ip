9113c4ccf42e119914cf2484afb6915bbee5313f
chee-yew <wongcheeyew1@gmail.com>
2026-08-24 02:23:56 +0800

    Implement level 2, adding list of tasks

--- src/main/java/WhimsyBot.java
+++ src/main/java/WhimsyBot.java
@@ -5,6 +5,7 @@ import java.util.Scanner;
  */
 public class WhimsyBot {
     private static final String SEPARATOR = "____________________________________________________________";
+    private static final int MAX_TASKS = 100;
 
     public static void main(String[] args) {
         String banner = " _       ___     _                         \n"
@@ -24,6 +25,8 @@ public class WhimsyBot {
         System.out.println("What can I do for you today?");
         System.out.println(SEPARATOR);
 
+        String[] tasks = new String[MAX_TASKS];
+        int taskCount = 0;
         Scanner scanner = new Scanner(System.in);
         while (scanner.hasNextLine()) {
             String command = scanner.nextLine();
@@ -35,7 +38,15 @@ public class WhimsyBot {
                 break;
             }
 
-            System.out.println(command);
+            if (command.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            } else if (taskCount < MAX_TASKS) {
+                tasks[taskCount] = command;
+                taskCount++;
+                System.out.println("added: " + command);
+            }
             System.out.println(SEPARATOR);
         }
     }
