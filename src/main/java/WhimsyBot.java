dc3871b87dff80fb5995277dd33cb041c897a162
chee-yew <wongcheeyew1@gmail.com>
2026-08-23 19:25:07 +0800

    Update code to greet user and exit

--- src/main/java/WhimsyBot.java
+++ src/main/java/WhimsyBot.java
@@ -2,6 +2,8 @@
  * Starts the Whimsy Bot chatbot application.
  */
 public class WhimsyBot {
+    private static final String SEPARATOR = "____________________________________________________________";
+
     public static void main(String[] args) {
         String banner = " _       ___     _                         \n"
                 + "| |     / / |__ (_)_ __ ___  ___ _   _     \n"
@@ -14,6 +16,12 @@ public class WhimsyBot {
                 + "|  _ \\ / _ \\| __|\n"
                 + "| |_) | (_) | |_ \n"
                 + "|____/ \\___/ \\__|\n";
+        System.out.println(SEPARATOR);
         System.out.println(banner);
+        System.out.println("Hello! I'm Whimsy Bot.");
+        System.out.println("What can I do for you?");
+        System.out.println(SEPARATOR);
+        System.out.println("Bye. Hope to see you again soon!");
+        System.out.println(SEPARATOR);
     }
 }
