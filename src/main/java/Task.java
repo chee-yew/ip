f53ae97887b0f556ed8ddddfe68a661d74c985af
chee-yew <wongcheeyew1@gmail.com>
2026-08-25 10:57:30 +0800

    Implement A-Classes extension, add task class

---
+++ src/main/java/Task.java
@@ -0,0 +1,40 @@
+/**
+ * Represents a task in Whimsy Bot's task list.
+ */
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    /**
+     * Creates an incomplete task with the given description.
+     *
+     * @param description the text describing the task
+     */
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    /**
+     * Returns the symbol used to show whether this task is complete.
+     *
+     * @return {@code "X"} when complete, otherwise a space
+     */
+    public String getStatusIcon() {
+        return isDone ? "X" : " ";
+    }
+
+    /**
+     * Marks this task as complete.
+     */
+    public void markAsDone() {
+        isDone = true;
+    }
+
+    /**
+     * Marks this task as incomplete.
+     */
+    public void unmarkAsDone() {
+        isDone = false;
+    }
+}
